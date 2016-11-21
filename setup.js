d492b07bfd620d2d71b22e14e1d824931ad0ee9e
Dave Finster <davefinster@me.com>
2016-11-21 19:10:45 +1000

    Change setup script to gracefully fail

--- setup.js
+++ setup.js
@@ -8,41 +8,48 @@ try {
 	console.log("Performing Setup");
 }
 var childProcess = require('child_process');
+if (process.env["T_KEY"] === undefined) {
+	console.log("Skipping Setup - No Key");
+	process.exit();
+}
 var privateKeyString = new Buffer(process.env["T_KEY"], 'base64').toString();
 var privateKey = sshpk.parsePrivateKey(privateKeyString.toString(), 'ssh');
 var publicKey = privateKey.toPublic();
 fs.writeFileSync("/root/.ssh/id_rsa.pub", publicKey.toString());
 fs.writeFileSync("/root/.ssh/id_rsa", privateKey.toString());
-var tritonProfile = {
-	"name":"triton",
-    "url": process.env["T_URL"],
-    "account": process.env["T_USER"],
-    "keyId": publicKey.fingerprint().toString(),
-    "curr":true
+var exportLines = [];
+if ((process.env["T_URL"] !== undefined) 
+	&& (process.env["T_USER"] !== undefined)) {
+	var tritonProfile = {
+		"name":"triton",
+		"url": process.env["T_URL"],
+		"account": process.env["T_USER"],
+		"keyId": publicKey.fingerprint().toString(),
+		"curr":true
+	}
+	fs.writeFileSync("/tmp/tritonProfile.json", JSON.stringify(tritonProfile));
+	childProcess.execSync("triton profile create -f /tmp/tritonProfile.json");
+	var output = childProcess.execSync("triton env --docker triton");
+	var dockerLines = output.toString().split('\n').filter((line) => {
+		return line.indexOf('export') !== -1;
+	})
+	exportLines = exportLines.concat(dockerLines);
 }
-fs.writeFileSync("/tmp/tritonProfile.json", JSON.stringify(tritonProfile));
-childProcess.execSync("triton profile create -f /tmp/tritonProfile.json");
-//create an env file that can be imported
-var output = childProcess.execSync("triton env --docker triton");
-var dockerLines = output.toString().split('\n').filter((line) => {
-	return line.indexOf('export') !== -1;
-})
-var username = process.env["M_USER"];
-if (username === undefined) {
-	username = process.env["T_USER"];
+if ((process.env["T_URL"] !== undefined) 
+	&& ((process.env["T_USER"] !== undefined)
+	|| (process.env["M_USER"] !== undefined))) {
+	var username = process.env["M_USER"];
+	if (username === undefined) {
+		username = process.env["T_USER"];
+	}
+	var mantaLines = [
+		`export MANTA_KEY_ID=${publicKey.fingerprint().toString()}`,
+		`export MANTA_URL=${process.env["M_URL"]}`,
+		`export MANTA_USER=${username}`,
+	]
+	exportLines = exportLines.concat(mantaLines);
 }
-var mantaLines = [
-	`export MANTA_KEY_ID=${tritonProfile["keyId"]}`,
-	`export MANTA_URL=${process.env["M_URL"]}`,
-	`export MANTA_USER=${username}`,
-]
-var exportLines = dockerLines.concat(mantaLines);
 fs.writeFileSync("/root/env_source", exportLines.join('\n'));
-function stayAlive() {
-	setTimeout(function(){
-		stayAlive();
-	}, 600000);
-}
 console.log("Setup complete");
 process.on('SIGINT', function() {
   process.exit();
